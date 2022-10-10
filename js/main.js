e1592800a672e086c0e7ffab840587bd4d800998
Борис <at9264839679@yandex.ru>
2022-10-10 17:29:24 +0300

    Переписал код создающий объект фотографии

--- js/main.js
+++ js/main.js
@@ -21,78 +21,16 @@ const checkMaxLengthString = (string, maxLength) =>
 
 checkMaxLengthString('', 5);
 
-const DESCRIPTIONS = [
-  'photo.1',
-  'photo.2',
-  'photo.3',
-  'photo.4',
-  'photo.5',
-  'photo.6',
-  'photo.7',
-  'photo.8',
-  'photo.9',
-  'photo.10',
-  'photo.11',
-  'photo.12',
-  'photo.13',
-  'photo.14',
-  'photo.15',
-  'photo.16',
-  'photo.17',
-  'photo.18',
-  'photo.19',
-  'photo.20',
-  'photo.21',
-  'photo.22',
-  'photo.23',
-  'photo.24',
-  'photo.25',
-];
+const SIMILAR_WIZARD_COUNT = 25;
 
-const SIMILAR_PHOTO_COUNT = 25;
+// Функция: Создания объекта фотографии
+const createPhoto = (id) => ({
+  id,
+  url: `photos/${id}.jpg`,
+  description: 'Описание фотографии',
+  likes: getRandomNumber(15, 200),
+  comments: getRandomNumber(0, 200)
+});
 
-// Функция: Создание массива элементов id
-const createId = () => {
-  const listsId = [];
+const createPhotos = Array.from({length: SIMILAR_WIZARD_COUNT}, (item, index) => createPhoto(index + 1));
 
-  for (let i = 1; i < SIMILAR_PHOTO_COUNT + 1; i++) {
-    const id = i;
-    listsId.push(id);
-  }
-  return listsId;
-};
-
-// Функция: Получение случайного элемента массива
-const getRandomElementArray = (elements) =>
-  elements[getRandomNumber(0, elements.length - 1)];
-
-// Функция: Получения нового массива из старого
-const shufflingArray = (array) => {
-  const mixArray = array.slice();
-  for (let i = mixArray.length - 1; i > 0; i--) {
-    const randomIndex = Math.floor(Math.random() * (i + 1));
-    const stepValue = mixArray[i];
-    mixArray[i] = mixArray[randomIndex];
-    mixArray[randomIndex] = stepValue;
-  }
-  return mixArray;
-};
-
-// Функция: Возвращающая массив объектов фотографий
-const createPhoto = () => {
-  const photos = [];
-  const photosId = shufflingArray(createId());
-
-  for (let i = 0; i < SIMILAR_PHOTO_COUNT; i++) {
-    photos.push({
-      id: photosId[i],
-      url: `photos/${photosId[i]}.jpg`,
-      description: getRandomElementArray(DESCRIPTIONS),
-      likes: getRandomNumber(15, 200),
-      comments: getRandomNumber(0, 200)
-    });
-  }
-  return photos;
-};
-
-createPhoto();
