051dd4c2fa64db341d104fe017286275cdd22224
Борис <at9264839679@yandex.ru>
2022-10-18 10:06:15 +0300

    Вернул константу и завернул в функцию блок создания картинок

--- js/picture.js
+++ js/picture.js
@@ -1,17 +1,23 @@
-import {createPhotos} from './data.js';
+import { createPhotos } from './data.js';
 
 const userPhotos = document.querySelector('.pictures');
 const picture = document.querySelector('#picture').content.querySelector('.picture');
 
-const similarPhotos = createPhotos(25);
+const similarPhotos = createPhotos();
+
 
 const similarListFragment = document.createDocumentFragment();
 
-similarPhotos.forEach(({url, likes, comments}) => {
-  const pictureElement = picture.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src = url;
-  pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__comments').textContent = comments;
-  similarListFragment.append(pictureElement);
-});
-userPhotos.append(similarListFragment);
+const createPictures = (element) => {
+
+  element.forEach(({ url, likes, comments }) => {
+    const pictureElement = picture.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = url;
+    pictureElement.querySelector('.picture__likes').textContent = likes;
+    pictureElement.querySelector('.picture__comments').textContent = comments;
+    similarListFragment.append(pictureElement);
+  });
+  userPhotos.append(similarListFragment);
+};
+
+createPictures(similarPhotos);
